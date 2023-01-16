52c7f6a17eaefe8aef4d875baac6de624e1ddfec
Micaela Dauria <micaelaschmidt47@gmail.com>
2023-01-16 16:56:13 -0300

    importing components to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
+import ContactList from "./ContactList";
+import AddContact from "./AddContact";
 
 function App() {
   return <div>Hello World</div>
