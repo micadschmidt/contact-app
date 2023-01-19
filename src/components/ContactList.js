71265eb91479556ce95719f367439f8e969acd58
Micaela Dauria <micaelaschmidt47@gmail.com>
2023-01-19 18:04:01 -0300

    contacts map

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,6 +1,18 @@
 import React from "react";
 
-const ContactList = () => {
+const ContactList = (props) => {
+  console.log(props);
+
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+      <div className="item">
+        <div className="content">
+          <div className="header">{contact.name}</div>
+          <div>{contact.email}</div>
+        </div>
+      </div>
+    )
+  })
   return (
     <div className="ui celled list">
       Contact List
