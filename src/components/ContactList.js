3897aca00b8ea948904f533f530f37ec652879de
Micaela Dauria <micaelaschmidt47@gmail.com>
2023-01-19 18:06:40 -0300

    contacts map function

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -10,9 +10,10 @@ const ContactList = (props) => {
           <div className="header">{contact.name}</div>
           <div>{contact.email}</div>
         </div>
+        <i className="trash alternate outline icon"></i>
       </div>
-    )
-  })
+    );
+  });
   return (
     <div className="ui celled list">
       Contact List
