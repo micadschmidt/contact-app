5840d46595b9a61d954ccfb396bdaa0c1f626e8a
Micaela Dauria <micaelaschmidt47@gmail.com>
2023-01-20 16:27:13 -0300

    contact card

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -4,7 +4,11 @@ const CardContact = () => {
 
   return (
     <div className='item'>
-      
+      <div className='content'>
+        <div className='header'>{contact.name}</div>
+        <div>{contact.email}</div>
+      </div>
+      <i className='trash alternate outline icon' style={{color:"red", marginTop:"7px"}}></i>
     </div>
   )
 
