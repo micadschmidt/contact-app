709af2475e892dcb284fdc98ac4d935397781116
Micaela Dauria <micaelaschmidt47@gmail.com>
2023-01-25 17:49:50 -0300

    const addcontacthandler

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from "./Header";
 import ContactList from "./ContactList";
