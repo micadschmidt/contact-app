c9d3f2024a813f7986e854a89f856d522a8c82ef
micadschmidt <micaelaschmidt47@gmail.com>
2023-02-17 09:48:33 -0300

    uuid install

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { uuid } from 'uuidv4';
 import './App.css';
 import Header from "./Header";
 import ContactList from "./ContactList";
@@ -10,7 +11,7 @@ function App() {
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, {id: uuid(), ...contacts }]);
   };
 
   useEffect(() => {
